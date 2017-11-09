7d73b0f3c877f551d945155e2b6af4511b009e77
maidou <maidou@anybeen.net>
2017-11-09 15:02:58 +0800

    Logs : toast utils

---
+++ app/src/main/java/com/dyzs/utils/ToastUtil.java
@@ -0,0 +1,27 @@
+package com.dyzs.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by maidou on 2015/3/24.
+ * 单例模式的 Toast
+ */
+public class ToastUtil {
+	private static Toast toast;
+	public static void makeText(Context context, String text){
+		if (toast == null) {
+			toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+		}
+		toast.setText(text);
+		toast.show();
+	}
+
+	public static void makeText(Context context,int text){
+		if (toast == null) {
+			toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+		}
+		toast.setText(text);
+		toast.show();
+	}
+}
