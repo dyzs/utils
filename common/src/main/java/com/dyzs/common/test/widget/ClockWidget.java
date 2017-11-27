5b8ea90170d875bc1079b410dc419cb283c65222
dyzs <627370404@qq.com>
2017-11-27 08:41:57 +0800

    you are the best thing that's happened to me in a long time 你是发生在我身上最美好的事情

---
+++ common/src/main/java/com/dyzs/common/test/widget/ClockWidget.java
@@ -0,0 +1,78 @@
+package com.dyzs.common.test.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import com.dyzs.common.R;
+import com.dyzs.common.utils.LogUtils;
+
+/**
+ * Created by NKlaus on 2017/11/26.
+ */
+
+public class ClockWidget extends AppWidgetProvider{
+    private static String TAG = ClockWidget.class.getSimpleName();
+    private AppWidgetManager mAppWidgetManager;
+    /*public ClockWidget() {
+        super();
+        LogUtils.i(TAG, "constructor.....");
+    }*/
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        LogUtils.i(TAG, "on receive.....");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        LogUtils.i(TAG, "on update.....");
+        ComponentName provider = new ComponentName(context, ClockWidget.class);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget_clock);
+        Intent intent = new Intent();
+        intent.setAction("com.dyzs.nurse.widget");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(),
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // views.setOnClickPendingIntent(R.id.ids, pendingIntent);
+
+        mAppWidgetManager = AppWidgetManager.getInstance(context);
+        mAppWidgetManager.updateAppWidget(provider, views);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        LogUtils.i(TAG, "on app widget options changed.....");
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        LogUtils.i(TAG, "on deleted.....");
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        LogUtils.i(TAG, "on enabled.....");
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        LogUtils.i(TAG, "on disabled.....");
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+    }
+}
