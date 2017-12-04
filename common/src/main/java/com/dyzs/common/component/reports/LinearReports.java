ecbebffbc09703fc5178c7ec42df624bc294c575
maidou <maidou@anybeen.net>
2017-12-04 09:49:32 +0800

    hold my hand you will not lost anymore

---
+++ common/src/main/java/com/dyzs/common/component/reports/LinearReports.java
@@ -0,0 +1,15 @@
+package com.dyzs.common.ui;
+
+/**
+ * @author dyzs
+ * Created on 2017/12/1.
+ */
+
+public class DataReports {
+
+
+
+
+
+
+}
---
+++ common/src/main/java/com/dyzs/common/component/reports/PancakeReports.java
@@ -0,0 +1,8 @@
+package com.dyzs.common.component.reports;
+
+/**
+ * Created by maidou on 2017/12/1.
+ */
+
+public class PancakeReports {
+}
