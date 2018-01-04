f1d7006bf7e6701020683cb2aa20cba3cf34625c
maidou <maidou@anybeen.net>
2018-01-04 17:01:46 +0800

    Logs : dotted line view add attrs

--- common/src/main/java/com/dyzs/common/ui/DottedLine.java
+++ common/src/main/java/com/dyzs/common/ui/DottedLine.java
@@ -20,9 +20,9 @@ import com.dyzs.common.R;
 
 public class DottedLine extends View{
     private Context mCtx;
-    private float mDashGap = 10f;
-    private float mDashWidth = 10f;
-    private int mType = 0;// 0 vertical, 1 horizontal
+    private float mDashGap;
+    private float mDashWidth;
+    private int mType = 0;// 0 vertical, 1 horizontal, 2 down tilt, 3 up tilt
     private Paint mPaint;
     private Path mPath;
     private float mWidth, mHeight;
@@ -45,6 +45,8 @@ public class DottedLine extends View{
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.DottedLine);
         mType = ta.getInteger(R.styleable.DottedLine_lineType, 0);
         mDotColor = ta.getColor(R.styleable.DottedLine_lineDotColor, ContextCompat.getColor(mCtx, R.color.oxygen_grey));
+        mDashGap = ta.getDimension(R.styleable.DottedLine_lineDashGap, 5f);
+        mDashWidth = ta.getDimension(R.styleable.DottedLine_lineDashWidth, 10f);
         ta.recycle();
 
         mPaint = new Paint();
@@ -52,7 +54,7 @@ public class DottedLine extends View{
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(5);
         mPaint.setColor(mDotColor);
-        PathEffect effects = new DashPathEffect(new float[] { 0, 3, 10, 0}, 1);
+        PathEffect effects = new DashPathEffect(new float[] { 0, mDashGap, mDashWidth, 0}, 1);
         mPaint.setPathEffect(effects);
     }
 
@@ -83,6 +85,18 @@ public class DottedLine extends View{
                 mPath = new Path();
                 mPath.moveTo(0, mHeight / 2);
                 mPath.lineTo(mWidth, mHeight / 2);
+                break;
+            case 2:
+                mPaint.setStrokeWidth(Float.parseFloat(Math.sqrt(mWidth * mWidth + mHeight * mHeight) + ""));
+                mPath = new Path();
+                mPath.moveTo(0, 0);
+                mPath.lineTo(mWidth, mHeight);
+                break;
+            case 3:
+                mPaint.setStrokeWidth(Float.parseFloat(Math.sqrt(mWidth * mWidth + mHeight * mHeight) + ""));
+                mPath = new Path();
+                mPath.moveTo(0, mHeight);
+                mPath.lineTo(mWidth, 0);
                 break;
         }
         canvas.drawPath(mPath, mPaint);
