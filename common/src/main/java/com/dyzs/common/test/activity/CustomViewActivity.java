65c3be075b9def38c9bbf8438a79c03e8ea06145
dyzs <627370404@qq.com>
2018-01-08 12:54:30 +0800

    Logs : adviser promotions digital capability physic surprise temporary capital currency cost sadistic relevant相关的 reveal揭示 coupon region named pipe fancy roof worm roped-off areas pitch沥青 barrels桶 familiar massive delegate curve coordinate authority parallel 平行 conqueror

--- common/src/main/java/com/dyzs/common/test/activity/CustomViewActivity.java
+++ common/src/main/java/com/dyzs/common/test/activity/CustomViewActivity.java
@@ -1,6 +1,8 @@
 package com.dyzs.common.test.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dyzs.common.R;
 import com.dyzs.common.base.BaseActivity;
@@ -9,21 +11,39 @@ import com.dyzs.common.base.BaseActivity;
  * Created by maidou on 2017/11/20.
  */
 
-public class CustomViewActivity extends BaseActivity {
+public class CustomViewActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_custion_view);
+        initView();
     }
 
     @Override
     public void initView() {
-
+        findViewById(R.id.solar_eclipse_loading).setOnClickListener(this);
+        findViewById(R.id.multi_player).setOnClickListener(this);
     }
 
     @Override
     public void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.solar_eclipse_loading:
+                intent = new Intent(this, CustomViewDisplayActivity.class);
+                intent.putExtra("viewName", "SolarEclipse");
+                break;
+            case R.id.multi_player:
+                intent = new Intent(this, CustomViewDisplayActivity.class);
+                intent.putExtra("viewName", "YinJiMultiPlayer");
+                break;
+        }
+        startActivity(intent);
     }
 }
---
+++ common/src/main/java/com/dyzs/common/test/activity/CustomViewDisplayActivity.java
@@ -0,0 +1,48 @@
+package com.dyzs.common.test.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
+import com.dyzs.common.R;
+import com.dyzs.common.base.BaseActivity;
+import com.dyzs.common.test.fragment.SolarEclipseFragment;
+import com.dyzs.common.test.fragment.YinJiMultiPlayerFragment;
+
+/**
+ * Created by maidou on 2018/1/8.
+ */
+
+public class CustomViewDisplayActivity extends BaseActivity{
+    FrameLayout container;
+    String viewName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_custion_view_display);
+        initView();
+    }
+
+
+    @Override
+    public void initView() {
+        container = (FrameLayout) findViewById(R.id.container);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_NONE);
+        viewName = getIntent().getStringExtra("viewName");
+        switch (viewName) {
+            case "SolarEclipse":
+                transaction.add(R.id.container, new SolarEclipseFragment(), viewName);
+                break;
+            case "YinJiMultiPlayer":
+                transaction.add(R.id.container, new YinJiMultiPlayerFragment(), viewName);
+                break;
+        }
+        transaction.commit();
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
