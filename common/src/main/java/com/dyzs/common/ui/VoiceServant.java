62b6ee9fdb38f557a46d1e9a93061509a59da2e5
dyzs <627370404@qq.com>
2018-01-13 23:12:24 +0800

    Logs : 颜色渐变值修改中

--- common/src/main/java/com/dyzs/common/ui/VoiceServant.java
+++ common/src/main/java/com/dyzs/common/ui/VoiceServant.java
@@ -8,16 +8,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.SweepGradient;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
 import android.view.animation.DecelerateInterpolator;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.OvershootInterpolator;
 
 import com.dyzs.common.R;
 
@@ -49,8 +47,9 @@ public class VoiceServant extends View{
     private Paint mMoriSummerPaint;// pointer paint
     private float mStartAngle;
     private int mDecibel = 119;// decibel
-    private int[] mGradientColors;
-    // add servant trick
+    private int[] mGalaxyColors;
+    private float[] mGalaxyPositions;
+
     public VoiceServant(Context context) {
         this(context, null);
     }
@@ -82,6 +81,7 @@ public class VoiceServant extends View{
         mTickWidth = 3f;
         mOxygenWidth = 30f;
         mMoriSummerWidth = 10f;
+        setGalaxyColors(mGalaxyColors);
 
         mDarkPaint = new Paint();
         mDarkPaint.setAntiAlias(true);
@@ -145,7 +145,11 @@ public class VoiceServant extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (mGalaxyColors.length != mGalaxyPositions.length) {
+            throw new IllegalArgumentException("length errors happened between colors and positions");
+        }
         drawDarkFlameMaster(canvas);
+        drawGalaxy(canvas);
     }
 
     private void drawDarkFlameMaster(Canvas canvas) {
@@ -153,14 +157,14 @@ public class VoiceServant extends View{
         canvas.drawCircle(mCircleCenter[0], mCircleCenter[1], mPointerRadius, mDarkPaint);
 
         /* draw tick mark, triangle mark and color pointer */
-        int dBPointer = (int) (mPointerDegree * 119 / mGalaxyDegree);
+        int dBPointer = (int) (mPointerDegree * mDecibel / mGalaxyDegree);
         for (int i = 0; i <= mDecibel; i++) {
             canvas.save();
             float rotateDegree;
             rotateDegree = mStartAngle + 90 + mAPieceOfDegree * i;
             canvas.rotate(rotateDegree, mCircleCenter[0], mCircleCenter[1]);
             if (i <= dBPointer) {
-                mFlamePaint.setColor(ContextCompat.getColor(mCtx, R.color.blair_grey));
+                mFlamePaint.setColor(getPointerColor(i));//ContextCompat.getColor(mCtx, R.color.blair_grey));
                 canvas.drawLine(
                         mCircleCenter[0],
                         mTickRectF.top - mTickLength / 2,
@@ -187,13 +191,15 @@ public class VoiceServant extends View{
             }
             canvas.restore();
         }
+    }
 
+    private void drawGalaxy(Canvas canvas) {
         /* draw colorful gradient */
         SweepGradient sweepGradient = new SweepGradient(
                 mCircleCenter[0],
                 mCircleCenter[1],
-                ContextCompat.getColor(mCtx, R.color.white),
-                ContextCompat.getColor(mCtx, R.color.oxygen_green));
+                mGalaxyColors,
+                mGalaxyPositions);
         mMasterPaint.setShader(sweepGradient);
         Path path = new Path();
         canvas.save();
@@ -204,30 +210,48 @@ public class VoiceServant extends View{
     }
 
     public void setPointerDecibel(int value) {
-        if (mDecibel == 119) {
-            value = value % 119;
-            float degree = mGalaxyDegree * value / 119;
-            this.mLastValue = mPointerDegree;
-            this.mPointerDegree = degree;
-            System.out.println("======= mLastValue:" + value);
-            System.out.println("======= mPointerDegree:" + value);
-            startPointerAnim();
-            // invalidate();
-        }
+        value = value % mDecibel;
+        float degree = mGalaxyDegree * value / mDecibel;
+        this.mLastValue = mPointerDegree;
+        this.mPointerDegree = degree;
+        System.out.println("======= mLastValue:" + value);
+        System.out.println("======= mPointerDegree:" + value);
+        startPointerAnim();
+        // invalidate();
     }
 
-    public int getPointerColor(int dBPointer) {
-        int sc = ContextCompat.getColor(mCtx, R.color.white);
-        int ec = ContextCompat.getColor(mCtx, R.color.oxygen_green);
+    public int getPointerColor(int pointerTick) {
+        int dBPointer = (int) (mPointerDegree * mDecibel / mGalaxyDegree);
+        // 计算当前指针于总数的比例;
+        float degreeRate = mGalaxyDegree / 360f;
+        float pointerDegreeRate = mPointerDegree / mGalaxyDegree * degreeRate;
+        int resSColor = ContextCompat.getColor(mCtx, R.color.white);
+        int resEColor = ContextCompat.getColor(mCtx, R.color.oxygen_green);
+        /*float rangeColorRate = 1f;
+        for (int i = 0 ; i < mGalaxyPositions.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            if (pointerDegreeRate < mGalaxyPositions[i]) {
+                float s = mGalaxyPositions[i-1];
+                float e = mGalaxyPositions[i];
+                rangeColorRate = (pointerDegreeRate - s) / (s - e);
+                resSColor = mGalaxyColors[i-1];
+                resEColor = mGalaxyColors[i];
+                break;
+            }
+        }*/
+        int sc = resSColor;
+        int ec = resEColor;
         int rS = Color.red(sc);
         int gS = Color.green(sc);
         int bS = Color.blue(sc);
         int rE = Color.red(ec);
         int gE = Color.green(ec);
         int bE = Color.blue(ec);
-        int r = (int) (rS + (rE - rS) * 1f / (mDecibel + 1) * dBPointer);
-        int g = (int) (gS + (gE - gS) * 1f / (mDecibel + 1) * dBPointer);
-        int b = (int) (bS + (bE - bS) * 1f / (mDecibel + 1) * dBPointer);
+        int r = (int) (rS + (rE - rS) * 1f / (mDecibel + 1) * pointerTick);
+        int g = (int) (gS + (gE - gS) * 1f / (mDecibel + 1) * pointerTick);
+        int b = (int) (bS + (bE - bS) * 1f / (mDecibel + 1) * pointerTick);
         return Color.argb(255, r, g, b);
     }
 
@@ -279,6 +303,47 @@ public class VoiceServant extends View{
     }
     public interface ServantListener {
         void startTension();
+    }
+
+    public int[] getGalaxyColors() {
+        return mGalaxyColors;
+    }
+
+    /**
+     * 设置颜色的时候同时计算 positions
+     */
+    public void setGalaxyColors(@Nullable int[] colors) {
+        if (colors == null) {
+            colors = new int[] {
+                    ContextCompat.getColor(mCtx, R.color.white),
+                    ContextCompat.getColor(mCtx, R.color.oxygen_green),
+                    ContextCompat.getColor(mCtx, R.color.cinnabar_red)
+            };
+        }
+        this.mGalaxyColors = colors;
+        setPositions(null);
+        invalidate();
+    }
+
+    /* not allow use */
+    private float[] getGalaxyPositions() {
+        return mGalaxyPositions;
+    }
+
+    /* not allow use */
+    private void setPositions(@Nullable float[] positions) {
+        float degreeRate = mGalaxyDegree / 360f;
+        if (positions == null) {
+            positions = new float[mGalaxyColors.length];
+            for (int i = 0; i < mGalaxyColors.length; i++) {
+                positions[i] = i * degreeRate / (mGalaxyColors.length - 1);
+            }
+        } else {// positions reset
+            for (int i = 0; i < positions.length; i++) {
+                positions[i] = positions[i] * degreeRate;
+            }
+        }
+        this.mGalaxyPositions = positions;
     }
 
     @Override
