8fd8a82363b2be00a98a2299dc702c7232fc500a
dyzs <627370404@qq.com>
2018-03-20 23:32:50 +0800

    it's a tough day

--- review/src/main/java/com/dyzs/review/designpattern/MyMain.java
+++ review/src/main/java/com/dyzs/review/designpattern/MyMain.java
@@ -4,7 +4,7 @@ import com.dyzs.review.designpattern.ch4multition.Emperor;
 
 public class MyMain {
     public static void main(String[] args) {
-        int ministerNum =10; //10个大臣
+        int ministerNum =12; //10个大臣
         for(int i=0; i<ministerNum; i++){
             Emperor emperor = Emperor.getInstance();
             System.out.print("第"+(i+1)+"个大臣参拜的是：");
