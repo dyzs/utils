61299d3b5ab247df5ab94c2c829363d0dc080059
dyzs <627370404@qq.com>
2018-05-05 21:12:51 +0800

    commit

---
+++ app/src/main/java/com/dyzs/app/base/adapter/BaseFragmentAdapter.java
@@ -0,0 +1,13 @@
+package com.dyzs.app.base.adapter;
+
+/**
+ * ================================================
+ *
+ * @author dyzs, Created on 2018/5/1.
+ *         <a href="dyzs.me@gmail.com">Contact me</a>
+ *         <a href="https://github.com/dyzs">Follow me</a>
+ *         ================================================
+ */
+
+public class BaseFragmentAdapter {
+}
